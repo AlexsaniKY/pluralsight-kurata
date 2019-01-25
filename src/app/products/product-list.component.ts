3ab3ca9f5c8de41a0750af9278ec0dc2e7d17e47
AlexsaniKY <alexsaniwilliams@gmail.com>
2019-01-25 14:29:39 -0500

    gave type inference to angular for json inspection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,12 +1,23 @@
 import {Component} from '@angular/core';
 import * as PRODUCTS from '../../api/products/products.json';
 
+interface ProductDefinition{
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    description: string;
+    price: number;
+    starRating: number;
+    imageUrl: string;
+}
+
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
 })
 export class ProductListComponent{
     pageTitle: string = 'Product List';
-    products: object[] = PRODUCTS.default;
+    products: ProductDefinition[] = <ProductDefinition[]> PRODUCTS.default;
 
 }
