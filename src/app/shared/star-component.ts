8255733dc75f0ffde07316da4ce3be0e35d2c46e
AlexsaniKY <alexsaniwilliams@gmail.com>
2019-01-28 13:58:27 -0500

    implemented lerp and color lerps

--- src/app/shared/star-component.ts
+++ src/app/shared/star-component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges } from "@angular/core";
+import { lerp_color } from 'src/helpers/lerp';
 
 @Component({
     selector: "pm-star",
@@ -12,6 +13,12 @@ export class StarComponent implements OnChanges {
 
     ngOnChanges(): void {
         this.starWidth = this.rating * 75 / 5;
+        let color_obj = lerp_color(
+            {r:255, g:255,   b:0}, 
+            {r:0,   g:0, b:255}, 
+            .5);
+        console.log(color_obj);
+        this.starColor = "rgb(" + color_obj.r + "," + color_obj.g + "," + color_obj.b + ")";
     }
 
     
---
+++ src/helpers/lerp.ts
@@ -0,0 +1,18 @@
+
+//from https://en.wikipedia.org/wiki/Linear_interpolation, protected from fp error,
+//does not bounds-check the mix to domain [0,1]
+export function lerp(num_min:number, num_max:number, mix:number): number{
+    return ((1-mix) * num_min) + (mix * num_max);
+
+}
+
+export function lerp_color(
+        col_1:{r:number,g:number,b:number}, 
+        col_2:{r:number,g:number,b:number}, 
+        mix:number){
+    return {
+        r: lerp(col_1.r,col_2.r,mix),
+        g: lerp(col_1.g,col_2.g,mix),
+        b: lerp(col_1.b,col_2.b,mix),
+    }
+}
