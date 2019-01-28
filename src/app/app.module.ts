8fd33860a17a6c39357df6fe6bd844c1b5650758
AlexsaniKY <alexsaniwilliams@gmail.com>
2019-01-28 10:54:46 -0500

    connected star component to list template and modules declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
+import { StarComponent } from './shared/star-component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    StarComponent,
   ],
   imports: [
     BrowserModule,
