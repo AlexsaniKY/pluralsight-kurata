c63ea4580f599de4d31b92da439c8962e1c5b931
AlexsaniKY <alexsaniwilliams@gmail.com>
2019-01-29 13:28:26 -0500

    changed onTitleClick to be clear it isnt the page title, cleanup and documentation

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,6 +14,11 @@ let empty_product:IProductDefinition = {
     imageUrl: "_"
 };
 
+/**
+ * 
+ * @description displays an interactive list of products, with title
+ * 
+ */
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
@@ -38,6 +43,7 @@ export class ProductListComponent implements OnInit{
     }
 
     private _listFilter: string= '';
+    /**@description a string to filter the products by.  Altering the value invokes the performFilter method */
     get listFilter(): string{
         return this._listFilter;
     }
@@ -47,10 +53,9 @@ export class ProductListComponent implements OnInit{
     }
 
     /**
-     * 
      * @param filterString user search string, containing separate search terms and quotations
      * @description filters the products list based on search criteria
-     * @returns a filtered copy of the products list
+     * @returns a filtered, shallow copy of the products list
      */
     performFilter(filterString: string): IProductDefinition[]{
         let filtered_list: IProductDefinition[] = this.products;
@@ -75,7 +80,12 @@ export class ProductListComponent implements OnInit{
         return filtered_list;
     }
 
-    onTitleClick(field: string){
+    /**
+     * @param field the name of the data member conforming to IProductDefinition
+     * @description initiates sorting by the selected values. If already 
+     * sorting by this value, reverses the sort direction
+     */
+    onHeaderClick(field: string){
         if(this.sortedBy === field) this.sortReverse = !this.sortReverse;
         else this.sortReverse = false;
         this.sortedBy = field;
@@ -83,27 +93,43 @@ export class ProductListComponent implements OnInit{
         this.sortProducts(field, this.sortReverse);
     }
 
+    /**
+     * @description sorts the products by the field, in ascending order if reverse
+     * @param field the data member to sort by
+     * @param reverse swaps order from ascending to descending
+     */
     sortProducts(field: string, reverse: boolean): void{
-        let compare;
+        let compare:(a: IProductDefinition, b: IProductDefinition) => number;
+        //if sorting by numeric
         if(typeof empty_product[field] === "number" ) 
-            compare = (a,b)=>{return a[field]-b[field]};
+            compare = (a,b)=>{
+                return a[field]-b[field];
+            };
+        //if sorting by date
         else if(empty_product[field] instanceof Date)
-            compare = (a,b)=>{return a[field].getTime() - b[field].getTime()};
+            compare = (a,b)=>{
+                return a[field].getTime() - b[field].getTime();
+            };
+        //if sorting by string. NOTE:any other types fall through to here.
         else compare = (a,b)=>{
-            let a_val = a[field];
-            let b_val = b[field];
-            if(a_val < b_val) return -1;
-            if(a_val > b_val) return 1;
+            if(a[field] < b[field]) return -1;
+            if(a[field] > b[field]) return 1;
             return 0;
         };
+        //reverse inputs if necessary
         if(reverse) this.filteredProducts.sort((a,b) => compare(b,a));
         else this.filteredProducts.sort(compare);
     }
 
+    /**
+     * @description describes what the value of the rating clicked was
+     * @param message string to add to title
+     */
     onRatingClicked(message: string): void{
         this.pageTitle = 'Product List: ' + message;
     }
 
+    /** @description toggles images beside products */
     togglePictures(): void{
         this.showPictures = !this.showPictures;
     }
